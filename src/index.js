df8bb1ef82a9d67ca80a17ea4ce593fdd2d5aef5
Nathan Faucett <nathanfaucett@gmail.com>
2015-02-18 10:06:45 -0600

    updates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-var bin = exports;
+var bin = exports,
+    NativeUint8Array = typeof(Uint8Array) !== "undefined" ? Uint8Array : Array;
 
 
 bin.stringToBytes = function(str) {
-    var bytes = [],
+    var length = str.length,
         i = -1,
-        il = str.length - 1;
+        il = length - 1,
+        bytes = new NativeUint8Array(length),
+        index = 0;
 
     while (i++ < il) {
-        bytes.push(str.charCodeAt(i) & 0xFF);
+        bytes[index] = str.charCodeAt(i) & 0xFF;
+        index += 1;
     }
 
     return bytes;
